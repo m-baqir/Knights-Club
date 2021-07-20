b39d35afd10986480b9152286c36029b0c1a9482
Suong <starolchen102@gmail.com>
2021-07-21 00:22:08 +0700

    Adding logic so that image gallery will seek out data from database

--- Suong-Image-Gallery/image_gallery.php
+++ Suong-Image-Gallery/image_gallery.php
@@ -1,9 +1,23 @@
+<?php
+
+use Webappdev\Knightsclub\models\Database;
+use Webappdev\Knightsclub\models\Images;
+  require_once '../vendor/autoload.php';
+
+  $gallery = new Images();
+
+  $db = Database::getDb();
+
+  $users = $gallery->getImagesById("2",$db); //for now user ID will be hardcoded
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
   <script type="text/javascript" async defer src="../js/sidenav.js"></script>
@@ -17,25 +31,37 @@
   <main>
     <div class="container">
       
-      <h1 ><a href="../user_profile_estevan/login_user.php" class="text-muted text-decoration-none">Profile</a></h1>
+      <h1 ><a href="../user_profile_estevan/login_user.php" class="text-muted text-decoration-none"><?=$users[0]->username; ?>'s Profile</a></h1>
     </div>
     
     <div id="carouselExampleIndicators" class="carousel slide container pt-2 pb-3" data-ride="carousel">     
       <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <?php
+          $count = 0;
+          foreach ($users as $user){ 
+            $isActive = "";
+            if($user->main_image == "1"){
+              $isActive = "class='active'";
+            } ?>
+            <li data-target="#carouselExampleIndicators" data-slide-to="<?= $count ?>" <?= $isActive?> ></li>
+          <?php 
+            $count++;
+          } ?>
+        <!-- <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li> -->
       </ol>
       <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img class="d-block w-100" src="./images/mateo-avila-chinchilla-x_8oJhYU31k-unsplash.jpg" alt="First slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="./images/eddy-lackmann-lLdGG3ESoiI-unsplash.jpg" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="./images/ben-sweet-2LowviVHZ-E-unsplash.jpg" alt="Third slide">
-        </div>
+      <?php 
+      
+      foreach ($users as $user){ 
+        $active = "";
+        if($user->main_image == "1"){
+          $active = "active";
+        }?>
+          <div class="carousel-item <?= $active ?>">
+            <img class="d-block w-100" src="./images/<?= $user->image_name ?>.<?= $user->picextension ?>"  alt="Image of user <?= $user->username; ?>">
+          </div>
+        <?php } ?>
       </div>
       <a class="carousel-control-prev w-25" href="#carouselExampleIndicators" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -51,38 +77,7 @@
       <button type="button" class="btn btn-outline-primary float-right">Add new picture</button>
     </div>
     </main>
-    <footer>
-      <div id="footer-menu">
-        <div>
-          <img id="footer-logo" src="./images/php-knights-logo.png" alt="site logo made of a knights helmet" />
-        </div>
-        <div class="footer-col">
-            <h3><a href="#">Home</a></h3>
-        </div>
-        <div class="footer-col">
-            <h3><a href="#">Search</a></h3>
-        </div>
-        <div class="footer-col">
-            <h3><a href="#">Profile</a></h3>
-            <ul>
-                <li><a href="#">Subscribe</a></li>
-            </ul>
-        </div>
-        <div class="footer-col">
-            <h3><a href="#">About Us</a></h3>
-            <ul>
-                <li><a href="#">FAQ</a></li>
-                <li><a href="#">Newsletter</a></li>
-                <li><a href="">Advice</a></li>
-                <li><a href="">Contact/Feedback</a></li>
-                <li><a href="">Site Map</a></li>
-            </ul>
-        </div>
-    </div>
-    <div id="copyright">
-        <p>&copy; 2021, PHP Knights</p>
-    </div>
-    </footer>
+    <?php require_once('../home_page/footer.php'); ?>
 </body>
 
 </html>
